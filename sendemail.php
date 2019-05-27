f1cf98fc5f162a57643bea882b740de4590390a6
BPM SAC <50600323+desarrolloBPM@users.noreply.github.com>
2019-05-27 14:26:48 -0500

    Add files via upload

--- sendemail.php
+++ sendemail.php
@@ -3,7 +3,7 @@ $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email'])); 
 $subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'grupo.asda.eirl@gmail.com';//replace with your email
+$to   		= 'ventas@bpmsac.com';//replace with your email
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
